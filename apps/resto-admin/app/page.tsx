387f55a0b81d1d86615e3149e332812e9151e56c
Gustavo Ezequiel Romero Canas <romeroc.gus@gmail.com>
2026-04-29 16:30:39 +0200

    update resto-admin app to fetch from prisma

--- apps/resto-admin/app/page.tsx
+++ apps/resto-admin/app/page.tsx
@@ -1,4 +1,27 @@
-export default function Home() {
+import { prisma } from '@repo/database';
+import { notFound } from 'next/navigation';
+import { Bar } from '@repo/shared-types';
+
+export default async function Home() {
+  const bar = (await prisma.bar.findUnique({
+    where: { slug: 'la-birreria' },
+    include: {
+      categories: {
+        orderBy: { order: 'asc' },
+        include: {
+          products: true,
+        },
+      },
+      products: {
+        orderBy: { name: 'asc' },
+      },
+    },
+  })) as Bar | null;
+  console.log('🚀 ~ bar:', bar);
+
+  if (!bar) {
+    notFound();
+  }
   return (
     <div>
       <main>Admin App</main>
