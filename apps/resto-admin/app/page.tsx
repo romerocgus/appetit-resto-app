e8731f3e159bc37dc6ff36e81784b3c1631adb06
Gustavo Ezequiel Romero Canas <romeroc.gus@gmail.com>
2026-05-14 17:58:03 +0200

    added shadcd and first component in resto-admin app

--- apps/resto-admin/app/page.tsx
+++ apps/resto-admin/app/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from '@repo/database';
 import { notFound } from 'next/navigation';
 import { Bar } from '@repo/shared-types';
+import { Button } from '@/components/ui/button';
 
 export default async function Home() {
   const bar = (await prisma.bar.findUnique({
@@ -29,6 +30,7 @@ export default async function Home() {
         <button className="bg-blue-700 text-blue-100 rounded-md hover:rounded-xl px-8 py-2 m-16px">
           Login
         </button>
+        <Button>Button ShadCn</Button>
       </main>
     </div>
   );
