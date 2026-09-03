bd4266223ca50ba3e4eb8a639098e0d8c5d947ff
Gustavo Ezequiel Romero Canas <romeroc.gus@gmail.com>
2026-09-03 17:51:40 +0200

    feat: Add internationalization support for Dashboard and Products pages

--- apps/resto-admin/app/[locale]/[barId]/dashboard/page.tsx
+++ apps/resto-admin/app/[locale]/[barId]/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import PageHeader from '@/app/ui/page-header';
 import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -11,9 +12,16 @@ export default async function DashboardPage({
   params: Promise<{ barId: string }>;
 }) {
   const { barId } = await params;
+  const t = await getTranslations('DashboardPage');
+  const pageTitle = t('title');
+  const pageDescription = t('description');
   return (
     <>
-      <PageHeader barId={barId} pageTitle="Dashboard" pageDescription="Test" />
+      <PageHeader
+        barId={barId}
+        pageTitle={pageTitle}
+        pageDescription={pageDescription}
+      />
       <h1>DASHBOARD</h1>
     </>
   );
--- apps/resto-admin/app/[locale]/[barId]/products/page.tsx
+++ apps/resto-admin/app/[locale]/[barId]/products/page.tsx
@@ -5,6 +5,7 @@ import { Button } from '@/components/ui/button';
 import { getProductsByBarId } from '@/lib/requests';
 import { Plus } from 'lucide-react';
 import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 
 export const metadata: Metadata = {
   title: 'Products',
@@ -17,12 +18,15 @@ export default async function ProductsPage({
 }) {
   const { barId } = await params;
   const products = await getProductsByBarId(barId);
+  const t = await getTranslations('ProductsPage');
+  const pageTitle = t('title');
+  const pageDescription = t('description');
   return (
     <>
       <PageHeader
         barId={barId}
-        pageTitle="Productos"
-        pageDescription="Aqui puedes gestionar tus productos"
+        pageTitle={pageTitle}
+        pageDescription={pageDescription}
       />
       <div className="flex flex-col gap-6 px-6 pb-10">
         <Button className="max-w-40">
